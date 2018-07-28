c2fae11693367492b1d94e594b8ab74b66aa33e1
tslazarov <lazarov.tsvetoslav@gmail.com>
2018-07-28 13:47:22 +0300

    Blood type selector

--- app/register/register-personal-information/register-personal-page.ts
+++ app/register/register-personal-information/register-personal-page.ts
@@ -1,11 +1,13 @@
 import { EventData } from "data/observable";
 import { Button } from "ui/button";
+import { Label } from "ui/label";
 import { Page } from "ui/page";
 
 import { RegisterPersonalViewModel } from "./register-personal-view-model";
 
 export function onNavigatingTo(args: EventData) {
     const page: Page = <Page>args.object;
+    page.addCssFile("./register/register-personal-information/register-personal-information.css");
     const context: any = page.navigationContext;
 
     console.log(context);
@@ -15,4 +17,19 @@ export function onNavigatingTo(args: EventData) {
 
 export function onRegisterTap(args: EventData): void { 
     console.log("tap");
+}
+
+export function onBloodTypeTap(args: EventData): void {
+    const label = <Label>args.object;
+    const wrapLayout = label.parent;
+    for(var i = 1; i <= 8; i += 1){
+        var childLabel = wrapLayout.getViewById(`option-${i}`);
+        childLabel.className = "blood-type-button";
+    }
+    label.className = "blood-type-button selected";
+
+    var myRegexp = /(.*)-([0-9]{1})/;
+    var match = myRegexp.exec(label.id);
+    
+    var selectedValue = match[2];
 }
